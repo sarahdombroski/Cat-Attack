53a6330015865b8e1e3ca8abbdf1ebe2a309a061
sarahdombroski <dom22003@byui.edu>
2025-11-06 10:49:45 -0700

    added api pulling picture

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-const apiKey = import.meta.env.api_key;
+// const apiKey = import.meta.env.api_key;
+const baseUrl = "https://api.thecatapi.com/v1/images/search";
+
+async function getPicture() {
+    try {
+        const response = await fetch(baseUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const image = document.querySelector("#mainImg");
+        image.src = data[0].url;
+
+        console.log(data);
+    } catch(error) {
+        console.error('There was an error:', error);
+    }
+}
+
+getPicture();
