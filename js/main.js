160a8d300a0420e97512dc5b66bd082b0f101572
sarahdombroski <dom22003@byui.edu>
2025-11-06 11:19:55 -0700

    updated getting and setting based on breed

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 // const apiKey = import.meta.env.api_key;
-const baseUrl = "https://api.thecatapi.com/v1/images/search";
+const breedUrl = "https://api.thecatapi.com/v1/breeds";
+const imgUrl = "https://api.thecatapi.com/v1/images/"
 
-async function getPicture() {
+async function setPicture(id) {
     try {
-        const response = await fetch(baseUrl);
+        const response = await fetch(imgUrl + id);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const data = await response.json();
-
-        const image = document.querySelector("#mainImg");
-        image.src = data[0].url;
-
         console.log(data);
+
+        // set image url
+        const image = document.querySelector("#mainImg");
+        image.src = data.url;
+        
     } catch(error) {
         console.error('There was an error:', error);
     }
 }
 
-getPicture();
+async function getInformation() {
+    try {
+        const response = await fetch(breedUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const array = await response.json();
+
+        // get random breed
+        const randomIndex = Math.floor(Math.random() * array.length);
+        const randomElement = array[randomIndex]; //object with all information for that breed
+
+        console.log(randomElement)
+
+        setPicture(randomElement.reference_image_id); // set the image on the main screen with the image id from the element
+    } catch(error) {
+        console.error('There was an error:', error);
+    }
+}
+
+
+getInformation();
